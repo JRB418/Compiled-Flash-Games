7ca3abe7eb3ae495cbb31cd4dfe01658642080ec
JRB418 <103917697+JRB418@users.noreply.github.com>
2023-12-01 14:41:42 -0500

    Update CamDumper.py

--- .github/workflows/CamDumper.py
+++ .github/workflows/CamDumper.py
@@ -1,3 +1,13 @@
+import os
+def(main):
+ print("Hellow from GitHub Actions!")
+ token = os.environ.get("AZURE_SECRET_TOKEN")
+ if not token:
+  raise RuntimeError("AZURE_SECRET_TOKEN env var is not set!")
+  print("All good! we found our env var")
+
+
+
 import requests, re
 from colorama import Fore, Back, Style
 print(Fore.LIGHTGREEN_EX +"""
